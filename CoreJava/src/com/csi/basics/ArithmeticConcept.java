6a3b1240510b0247ec3894d43f8184c0c50149da
shashankmane19 <shashiimane19@gmail.com>
2021-11-27 22:31:24 +0530

    FIFTH

---
+++ CoreJava/src/com/csi/basics/ArithmeticConcept.java
@@ -0,0 +1,20 @@
+package com.csi.basics;
+
+public class ArithmeticConcept {
+	public static void main(String[] args) {
+
+		int n1 = 20, n2 = 10, result = 0;
+
+		result = n1 + n2;
+		System.out.println("\n Addition Result: " + result);
+
+		result = n1 - n2;
+		System.out.println("\n Substraction Result: " + result);
+
+		result = n1 * n2;
+		System.out.println("\n Multiplication Result: " + result);
+
+		result = n1 / n2;
+		System.out.println("\n Division Result: " + result);
+	}
+}
